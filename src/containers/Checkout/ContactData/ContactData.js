78f678a4f7d733ed8957014d64121cc3aa3772bd
Atanasov-A <thestubborn@abv.bg>
2021-04-14 12:24:11 +0200

    Cleaned some code

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,17 +1,17 @@
-import {useState} from "react";
+import { useRef, useState } from "react";
 import "./ContactData.css";
 
-import {Button} from "../../../components/UI/Button/Button";
-import {axiosInstance} from "../../../axios-orders";
-import {Spinner} from "../../../components/UI/Spinner/Spinner";
-import {useHistory} from "react-router";
+import { Button } from "../../../components/UI/Button/Button";
+import { axiosInstance } from "../../../axios-orders";
+import { Spinner } from "../../../components/UI/Spinner/Spinner";
+import { useHistory } from "react-router";
 const ContactData = (props) => {
-  const {ingredients, price} = props;
+  const { ingredients, price } = props;
 
-  const [userName, setUserName] = useState();
-  const [email, setEmail] = useState();
-  const [street, setStreet] = useState();
-  const [postalCode, setPostalCode] = useState();
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [street, setStreet] = useState("");
+  const [postalCode, setPostalCode] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
 
@@ -23,21 +23,26 @@ const ContactData = (props) => {
       ingredients: ingredients,
       price: price,
       customer: {
-        name: "My name",
+        name: userName,
         address: {
-          street: "My street",
-          zipCode: 666,
+          street: street,
+          zipCode: postalCode,
           country: "DE",
         },
-        email: "my@mymail.com",
+        email: email,
         deliveryMethod: "fastest",
       },
     };
+
     axiosInstance
       .post("/orders.json", order)
       .then((response) => {
+        setUserName("");
+        setStreet("");
+        setEmail("");
+        setPostalCode("");
         setLoading(false);
-        history.push('/');
+        history.push("/");
       })
       .catch((err) => {
         setLoading(false);
@@ -47,19 +52,47 @@ const ContactData = (props) => {
   return (
     <div className="ContactData">
       <h4>Enter your contact data</h4>
-      { loading ? <Spinner /> :
-        <form>
-          <input type="text" name="username" placeholder="Your username" />
-          <input type="text" name="email" placeholder="Your email" />
-          <input type="text" name="street" placeholder="Your street" />
-          <input type="text" name="postalCode" placeholder="Your postal code" />
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <form onSubmit={() => console.log("submitted")}>
+            <input
+              type="text"
+              name="username"
+              placeholder="Your username"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="text"
+              name="street"
+              placeholder="Your street"
+              value={street}
+              onChange={(e) => setStreet(e.target.value)}
+            />
+            <input
+              type="text"
+              name="postalCode"
+              placeholder="Your postal code"
+              value={postalCode}
+              onChange={(e) => setPostalCode(e.target.value)}
+            />
+          </form>
           <Button btnType="Success" clicked={orderHandler}>
             ORDER
-        </Button>
-        </form>
-      }
+          </Button>
+        </>
+      )}
     </div>
   );
 };
 
-export {ContactData};
+export { ContactData };
