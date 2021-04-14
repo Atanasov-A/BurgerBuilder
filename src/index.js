487fb4e2ce2caef7e6247887db9ac50d887e3bf8
Atanasov-A <thestubborn@abv.bg>
2021-04-14 12:38:39 +0200

    Fixed deployment

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
 const MyApp = (
-  <BrowserRouter>
+  <BrowserRouter basename="/BurgerBuilder">
     <App />
   </BrowserRouter>
 );
